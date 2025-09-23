ab682499391e32dd139e7798ab4264a85ed7dcf8
SerhiiK1 <sergij.nv@icloud.com>
2025-09-23 18:08:07 -0300

    Small change
    small changes

--- app/src/main/java/ca/unb/mobiledev/studyhub/LoginPage.kt
+++ app/src/main/java/ca/unb/mobiledev/studyhub/LoginPage.kt
@@ -3,7 +3,6 @@ package ca.unb.mobiledev.studyhub
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
