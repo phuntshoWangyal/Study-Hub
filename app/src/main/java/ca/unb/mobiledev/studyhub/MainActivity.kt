501478907fd7cd967c8ed8316d17badd5ed5d030
Phuntsho Wangyal Galay <12210078.gcit@rub.edu.bt>
2025-09-25 18:05:26 -0300

    Added the Sign Up page

--- app/src/main/java/ca/unb/mobiledev/studyhub/MainActivity.kt
+++ app/src/main/java/ca/unb/mobiledev/studyhub/MainActivity.kt
@@ -2,10 +2,20 @@ package ca.unb.mobiledev.studyhub
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_main)
+
+        val signupButton : Button = findViewById(R.id.signupButton)
+
+        signupButton.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/ca/unb/mobiledev/studyhub/SignupActivity.kt
@@ -0,0 +1,12 @@
+package ca.unb.mobiledev.studyhub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup_main)
+
+    }
+}
