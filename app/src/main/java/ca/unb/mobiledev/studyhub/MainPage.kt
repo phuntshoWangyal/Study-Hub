b9b3d951fe49f1aaa540680944fd0fadf3c178ac
Tony Pham <tonypm102@gmail.com>
2025-09-27 00:56:15 -0300

    Change nav/delete pages and change ids around

--- app/src/main/java/ca/unb/mobiledev/studyhub/MainPage.kt
+++ app/src/main/java/ca/unb/mobiledev/studyhub/MainPage.kt
@@ -1,40 +1,51 @@
 package ca.unb.mobiledev.studyhub
 
-import android.content.Intent
-import android.os.Bundle
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.lang.Exception
 
 class MainPage : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.main_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_page)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Load default fragment
+        loadFragment(home_fragment())
+
+        // Bottom Navigation setup
+        bottomNav = findViewById(R.id.bottomNav)
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.classAddButton -> {
+                    loadFragment(home_fragment())
+                    true
+                }
+                R.id.settingButton -> {
+                    loadFragment(setting_fragment())
+                    true
+                }
+                R.id.rankingButton -> {
+                    loadFragment(rank_fragment())
+                    true
+                }
+                else -> false
+            }
         }
+    }
 
-
-        val rankingButton: Button = findViewById(R.id.rankingButton)
-        rankingButton.setOnClickListener {
-            val intent = Intent(this@MainPage, RankingPage::class.java)
-            startActivity(intent)
-        }
-
-        val addClassButton: Button = findViewById(R.id.classAddButton)
-        addClassButton.setOnClickListener {
-
-        }
-
-        val settingsButton: Button = findViewById(R.id.settingsButton)
-        settingsButton.setOnClickListener {
-            val intent = Intent(this@MainPage, SettingsPage::class.java)
-            startActivity(intent)
-        }
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
     }
 }
--- app/src/main/java/ca/unb/mobiledev/studyhub/rank_fragment.kt
+++ app/src/main/java/ca/unb/mobiledev/studyhub/rank_fragment.kt
@@ -13,11 +13,11 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [setting.newInstance] factory method to
+ * Use the [ranking.newInstance] factory method to
  * create an instance of this fragment.
  */
-class setting : Fragment() {
-    // TODO: Rename and chGITange types of parameters
+class rank_fragment : Fragment() {
+    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -34,7 +34,7 @@ class setting : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        return inflater.inflate(R.layout.fragment_ranking, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class setting : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment setting.
+         * @return A new instance of fragment ranking.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            setting().apply {
+            rank_fragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
